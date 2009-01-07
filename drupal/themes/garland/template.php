1a74ea3b455f26bebc813bd7ccdb00cabc21b84d
n.meyer@usa.net <n.meyer@usa.net@944859c6-917e-11dd-b261-6159a506a750>
2009-01-07 06:43:08 +0000

    Tighten up logic renaming question-node titles. Add custom EP javascript file and function to scale images on the page

--- drupal/themes/garland/template.php
+++ drupal/themes/garland/template.php
@@ -2,15 +2,17 @@
 // $Id: template.php,v 1.16 2007/10/11 09:51:29 goba Exp $
 // Dup of Garland 11/2008 with Drupal 6.4
 
-drupal_add_css(path_to_theme().'/elephant-parents.css');
-drupal_add_js(path_to_theme().'/elephant-parents.js');
+drupal_add_css(path_to_theme().'/includes/elephant-parents.css');
+drupal_add_js(path_to_theme().'/includes/jquery-accordion/jquery.accordion.js'); //jquery.accordion.min.js');
+drupal_add_js(path_to_theme().'/includes/elephant-parents.js');
+
 
 function phptemplate_preprocess(&$variables, $hook) 
 {
 	global $user;
 	// give questions a friendly title and preproc comments below
-	if($hook == 'node' || $hook == 'page' && $variables['node']->type == 'question') {
-		$variables['title'] = 'Question: '. $variables['title'];
+	if(($hook == 'node' || $hook == 'page') && $variables['node']->type == 'question') {
+		$variables['title'] = 'Q/A: '. $variables['title'];
 	}
 }
 
@@ -37,6 +39,18 @@ function phptemplate_body_class($left, $right) {
 }
 
 /**
+ * Generates IE CSS links for LTR and RTL languages.
+ */
+function phptemplate_get_ie_styles() {
+	global $language;
+	$iecss = '<link type="text/css" rel="stylesheet" media="all" href="'. base_path() . path_to_theme() .'/fix-ie.css" />';
+	if (defined('LANGUAGE_RTL') && $language->direction == LANGUAGE_RTL) {
+		$iecss .= '<style type="text/css" media="all">@import "'. base_path() . path_to_theme() .'/fix-ie-rtl.css";</style>';
+	}
+	return $iecss;
+}
+
+/**
  * Return a themed breadcrumb trail.
  */
 function phptemplate_breadcrumb($breadcrumb) {
@@ -44,6 +58,39 @@ function phptemplate_breadcrumb($breadcrumb) {
 		return '<div class="breadcrumb">'. implode(' › ', $breadcrumb) .'</div>';
 	}
 }
+
+/**
+ * Override or insert PHPTemplate variables into the templates.
+ */
+function phptemplate_preprocess_page(&$vars) {
+	$vars['tabs2'] = menu_secondary_local_tasks();
+	// Hook into color.module
+	if (module_exists('color')) {
+		_color_page_alter($vars);
+	}
+	// collect comments for themeing, unset them in the preprocess_node
+	// http://drupal.org/node/161139 //http://drupal.org/node/122240#comment-272809
+}
+
+function phptemplate_preprocess_node(&$vars) {
+	// $vars['node']->comment = 0;
+}
+
+function phptemplate_comment_submitted($comment) {
+	return t('!datetime — !username',
+		array(
+			'!username' => theme('username', $comment),
+			'!datetime' => format_date($comment->timestamp)
+		));
+}
+function phptemplate_node_submitted($node) {
+	return t('!datetime — !username',
+		array(
+			'!username' => theme('username', $node),
+			'!datetime' => format_date($node->created),
+		));
+}
+
 
 /**
  * Allow themable wrapping of all comments.
@@ -57,67 +104,11 @@ function phptemplate_comment_wrapper($content, $node) {
 	}
 	return '<div id="comments"><h2 class="comments">'. t('Comments') .'</h2>'. $content .'</div>';
 }
-
-/**
- * Override or insert PHPTemplate variables into the templates.
- */
-function phptemplate_preprocess_page(&$vars) {
-	$vars['tabs2'] = menu_secondary_local_tasks();
-
-	// Hook into color.module
-	if (module_exists('color')) {
-		_color_page_alter($vars);
-	}
-	
-	// collect comments for themeing, unset them in the preprocess_node
-	// http://drupal.org/node/161139 //http://drupal.org/node/122240#comment-272809
-}
-
-function phptemplate_preprocess_node(&$vars) {
-	// $vars['node']->comment = 0;
-}
-
-/**
- * Returns the rendered local tasks. The default implementation renders
- * them as tabs. Overridden to split the secondary tasks.
- */
-function phptemplate_menu_local_tasks() {
-	return menu_primary_local_tasks();
-}
-
-function phptemplate_comment_submitted($comment) {
-	return t('!datetime — !username',
-		array(
-			'!username' => theme('username', $comment),
-			'!datetime' => format_date($comment->timestamp)
-		));
-}
-
-function phptemplate_node_submitted($node) {
-	return t('!datetime — !username',
-		array(
-			'!username' => theme('username', $node),
-			'!datetime' => format_date($node->created),
-		));
-}
-
-/**
- * Generates IE CSS links for LTR and RTL languages.
- */
-function phptemplate_get_ie_styles() {
-	global $language;
-	$iecss = '<link type="text/css" rel="stylesheet" media="all" href="'. base_path() . path_to_theme() .'/fix-ie.css" />';
-	if (defined('LANGUAGE_RTL') && $language->direction == LANGUAGE_RTL) {
-		$iecss .= '<style type="text/css" media="all">@import "'. base_path() . path_to_theme() .'/fix-ie-rtl.css";</style>';
-	}
-	return $iecss;
-}
-
 /**
  * Hide the node comment box for most user-roles
  */
 function phptemplate_box($title = '', $content = '', $region = 'main') {
-	// hide the comment form on the node page for non-admins
+	// hide the page comment form for non-admins, anyone can reply to a comment
 	if($title == 'Post new comment') {
 		if(!user_access('administer comments') && !(arg(0) == 'comment' && arg(1) == 'reply')) { 
 			return ''; 
@@ -126,6 +117,14 @@ function phptemplate_box($title = '', $content = '', $region = 'main') {
 	}
 
 	return '<h2 class="title">'. $title .'</h2><div>'. $content .'</div>';
+}
+
+/**
+ * Returns the rendered local tasks. The default implementation renders
+ * them as tabs. Overridden to split the secondary tasks.
+ */
+function phptemplate_menu_local_tasks() {
+	return menu_primary_local_tasks();
 }
 
 /**
@@ -146,8 +145,4 @@ function phptemplate_menu_item($link, $has_children, $menu = '', $in_active_trai
 	}
 	return '<li class="'. $class .'">'. $link . $menu ."</li>\n";
 }
-
-
-
-
 
